f1622ff2ad6293a0226bf76ad288ddc9badd4a31
Lord-Of-WuTang <95978653+Lord-Of-WuTang@users.noreply.github.com>
2022-05-16 13:34:13 +0100

    Add files via upload

---
+++ Capstone.js
@@ -0,0 +1,19 @@
+function ValidateEmail(mail) {
+  if (
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)
+  ) {
+    return true;
+  }
+  alert("You have entered an invalid email address!");
+  return false;
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
